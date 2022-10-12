b78a753b294d4bffef55d9d10b98d2a288da616c
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-12 19:59:18 +0000

    fixed a link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -26,7 +26,7 @@ const toggleLocales = () => {
       <div i-carbon-dicom-overlay />
     </RouterLink>
 
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/Dragonlord1005/vitesse-boi" target="_blank" title="GitHub">
       <div i-carbon-logo-github />
     </a>
   </nav>
