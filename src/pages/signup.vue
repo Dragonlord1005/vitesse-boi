aa2c9be88ef39121164284ca3711350f6737eabf
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-17 15:20:34 +0000

    stuff

---
+++ src/pages/signup.vue
@@ -0,0 +1,15 @@
+<!-- We will be using netlify for form submission -->
+
+<template>
+  <div>
+    <form netlfiy netlify-honeypot="bot-field" hidden>
+      <label>Name: </label><input type="text" name="name">
+      <br>
+      <label>Email: </label><input type="email" name="email">
+      <br>
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
