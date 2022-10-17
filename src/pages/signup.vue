0c157113d38109a0594d5fd710c6cbf06420ca9f
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-17 20:19:28 +0000

    sup

--- src/pages/signup.vue
+++ src/pages/signup.vue
@@ -1,15 +1,30 @@
-<!-- We will be using netlify for form submission -->
+<!-- We are using netlify identity for authentication -->
+<script>
+import netlifyIdentity from 'netlify-identity-widget'
+
+netlifyIdentity.init({
+  // APIUrl: 'https://vitesse-boi.netlify.app/',
+})
+// netlifyIdentity.open
+
+export default {
+  name: 'Signup',
+  methods: {
+    signup() {
+      netlifyIdentity.open('signup')
+    },
+  },
+}
+</script>
 
 <template>
   <div>
-    <form netlify netlify-honeypot="bot-field">
-      <label>Name: </label><input type="text" name="name">
-      <br>
-      <label>Email: </label><input type="email" name="email">
-      <br>
-      <button type="submit">
-        Submit
-      </button>
-    </form>
+    <!-- <div data-netlify-identity-menu />
+    <div data-netlify-identity-button>
+      Login with Netlify Identity
+    </div> -->
+    <button @click="signup">
+      Sign Up
+    </button>
   </div>
 </template>
